5149a6686511717cbe6d50844d3cf56047cd99c2
Thanos Zolotas <zolotas4@gmail.com>
2017-11-28 12:01:12 +0000

    Removed ignored files

--- XamarinMHA/XamarinMHA/LoginPage.xaml.cs
+++ XamarinMHA/XamarinMHA/LoginPage.xaml.cs
@@ -32,21 +32,48 @@ namespace HelloWorld
         {
             HttpClient oHttpClient = new HttpClient();
             string url = "http://10.0.3.2:8080/people/search/findByUserName?username=" + usernameEntry.Text;
-            Debug.WriteLine(url);
-            var response = await oHttpClient.GetStringAsync(url);
-            Person user = JsonConvert.DeserializeObject<Person>(response);
-            if (String.Equals(passwordEntry.Text, user.Password))
+            var response = await oHttpClient.GetAsync(url);
+            if (response.IsSuccessStatusCode)
             {
-                UserHomePage uh = new UserHomePage
+                // Username found in users' DB.
+                Person user = JsonConvert.DeserializeObject<Person>(await response.Content.ReadAsStringAsync());
+                if (String.Equals(passwordEntry.Text, user.Password))
                 {
-                    BindingContext = user
-                };
-                await Navigation.PushAsync(uh);
+                    UserHomePage uh = new UserHomePage
+                    {
+                        BindingContext = user
+                    };
+                    await Navigation.PushAsync(uh);
+                }
+                else
+                {
+                    invalidPasswordLabel.IsVisible = true;
+                }
             } else
             {
-                invalidPasswordLabel.IsVisible = true;
+                url = "http://10.0.3.2:8080/mentors/search/findByUserName?username=" + usernameEntry.Text;
+                response = await oHttpClient.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    // Username found in mentors' DB.
+                    Mentor mentor = JsonConvert.DeserializeObject<Mentor>(await response.Content.ReadAsStringAsync());
+                    if (String.Equals(passwordEntry.Text, mentor.Password))
+                    {
+                        MentorHomePage mh = new MentorHomePage
+                        {
+                            BindingContext = mentor
+                        };
+                        await Navigation.PushAsync(mh);
+                    }
+                    else
+                    {
+                        invalidPasswordLabel.IsVisible = true;
+                    }
+                } else
+                {
+                    await DisplayAlert("Error", "Something went wrong: no user found or access to DB is not possible.", "OK");
+                }
             }
-                
         }
     }
 }
---
+++ XamarinMHA/XamarinMHA/MentorHomePage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MentorHomePage : ContentPage
+    {
+        Mentor mentor;
+
+        public MentorHomePage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+            mentor = (Mentor)BindingContext;
+            Debug.WriteLine(mentor.FirstLastName);
+        }
+    }
+}
