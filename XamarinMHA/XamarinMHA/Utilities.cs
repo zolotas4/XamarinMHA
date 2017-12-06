3dfe154c338c148463a0f77328bc86ae07a555a2
Thanos Zolotas <zolotas4@gmail.com>
2017-12-06 15:53:03 +0000

    Added utility methods to construct DateTime based on slot number, get the time of each slot number and filter the availability of slots based on duration.

--- XamarinMHA/XamarinMHA/Utilities.cs
+++ XamarinMHA/XamarinMHA/Utilities.cs
@@ -9,11 +9,61 @@ namespace HelloWorld
 {
     public static class Utilities
     {
-        public static String LOCALHOST = "192.168.1.3";
+        public static String LOCALHOST = "192.168.1.120";
         //public static String LOCALHOST = "10.0.3.2";
         public static void toggleSpinner (ActivityIndicator spinner) {
             spinner.IsVisible = !spinner.IsVisible;
             spinner.IsRunning = !spinner.IsRunning;
+        }
+
+        public static DateTime FindDateTimeBasedOnSlotNumber(DateTime date, int slotNumber)
+        {
+            DateTime completeDate;
+            if (slotNumber % 2 == 0)
+            {
+                return completeDate = date.AddHours(slotNumber / 2);
+            } else
+            {
+                return completeDate = date.AddHours(slotNumber / 2).AddMinutes(30);
+            }
+        }
+
+        public static String FindTimeBaseOnSlotNumber(int slotNumber)
+        {
+            if (slotNumber % 2 == 0)
+            {
+                if (slotNumber/2 < 10)
+                {
+                    return "0" + (slotNumber / 2).ToString() + ":00";
+                } else
+                {
+                    return (slotNumber / 2).ToString() + ":00";
+                }
+            }
+            else
+            {
+                return (slotNumber / 2) + ":30";
+            }
+        }
+
+        public static List<int> filterAvailableSlotsBasedOnDuration(List<int> allAvailableSlots, int duration)
+        {
+            List<int> filteredSlots = new List<int>();
+            foreach (int aSlot in allAvailableSlots)
+            {
+                Boolean flag = true;
+                for (int i = aSlot; i <= aSlot + duration; i++) {
+                    if (!allAvailableSlots.Contains(i))
+                    {
+                        flag = false;
+                    }
+                }
+                if (flag)
+                {
+                    filteredSlots.Add(aSlot);
+                }
+            }
+            return filteredSlots;
         }
     }
 }
