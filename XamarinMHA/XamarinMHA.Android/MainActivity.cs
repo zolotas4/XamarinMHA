1a4dc55c84bc750ff17180b2a4b2a4b0d8630b4f
Thanos Zolotas <zolotas4@gmail.com>
2018-02-01 12:22:17 +0000

    Changed the name of the application to Keyfort Apps

--- XamarinMHA/XamarinMHA.Android/MainActivity.cs
+++ XamarinMHA/XamarinMHA.Android/MainActivity.cs
@@ -11,7 +11,7 @@ using Plugin.Permissions.Abstractions;
 
 namespace XamarinMHA.Droid
 {
-    [Activity(Label = "XamarinMHA", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Keyfort Apps", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
